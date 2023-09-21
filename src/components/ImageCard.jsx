48b670964412bdcfa055876297c7a8f31253b833
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-09-21 18:20:19 +0100

    added loading component

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DndContext, closestCenter } from '@dnd-kit/core';
 import { SortableContext, arrayMove, horizontalListSortingStrategy, useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import Header from './Header';
+import LoadingSpinner from './LoadingSpinner';
 
 const initialCardState = [
     {
@@ -78,89 +79,84 @@ const initialCardState = [
 ];
 
 const SortableCard = ({ cardItem }) => {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-  } = useSortable({ id: cardItem.id });
-  const style = {
-    transition,
-    transform: CSS.Transform.toString(transform),
-  };
-  return (
-    <div className='image-card'>
-      <div
-        ref={setNodeRef}
-        style={style}
-        {...attributes}
-        {...listeners}
-        className="image-card-content">
-        <div className=''>
-          <img
-            className='w-[200px] h-[150px] md:h-[230px] rounded transform hover:scale-110 transition-transform duration-300'
-            src={cardItem.image}
-            alt={cardItem.tag} />
-          <p className='relative top-[-40px] left-2 bg-green-300 text-black text-sm bg-opacity-50 w-fit rounded-3xl px-4 py-1'>{cardItem.tag}</p>
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition,
+    } = useSortable({ id: cardItem.id });
+    const style = {
+        transition,
+        transform: CSS.Transform.toString(transform),
+    };
+    return (
+        <div className='image-card'>
+            <div
+                ref={setNodeRef}
+                style={style}
+                {...attributes}
+                {...listeners}
+                className="image-card-content">
+                <div className=''>
+                    <img
+                        className='w-[200px] h-[150px] md:h-[230px] rounded transform hover:scale-110 transition-transform hover:shadow-2xl duration-300'
+                        src={cardItem.image}
+                        alt={cardItem.tag} />
+                    <p className='relative top-[-40px] left-2 bg-green-300 text-black text-sm bg-opacity-50 w-fit rounded-3xl px-4 py-1'>{cardItem.tag}</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 function ImageCard() {
-  const [items, setItems] = useState(initialCardState);
-  const [searchTerm, setSearchTerm] = useState('');
+    const [items, setItems] = useState(initialCardState);
+    const [loading, setLoading] = useState(true);
 
-  const onDragEnd = (event) => {
-    const { active, over } = event;
-    if (active.id === over.id) {
-      return;
-    }
-    setItems((cards) => {
-      const oldIndex = cards.findIndex((cardItem) => cardItem.id === active.id);
-      const newIndex = cards.findIndex((cardItem) => cardItem.id === over.id);
-      return arrayMove(cards, oldIndex, newIndex)
-    });
-    console.log('Drag ended:', event);
-  };
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
 
-  // Filter the cards based on the search term
-  const filteredItems = items.filter((cardItem) =>
-    cardItem.tag.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+    const onDragEnd = (event) => {
+        const { active, over } = event;
+        if (active.id === over.id) {
+            return;
+        }
+        setItems((cards) => {
+            const oldIndex = cards.findIndex((cardItem) => cardItem.id === active.id);
+            const newIndex = cards.findIndex((cardItem) => cardItem.id === over.id);
+            return arrayMove(cards, oldIndex, newIndex)
+        });
+        console.log('Drag ended:', event);
+    };
 
-  return (
-    <div>
-      <Header />
-      <p className='text-3xl font-bold text-center mt-4'>Image Gallery</p>
-      <div className='w-[300px] p-4 m-auto'>
-          <input
-            type="text"
-            placeholder="Search by tag"
-            className="w-[300px] m-auto p-2 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          {/* <button className='bg-black px-4 py-1 text-white rounded relative top-[-37px] left-[212px]'>search</button> */}
+    return (
+        <div>
+            <Header />
+            <p className='text-3xl font-bold text-center mt-4'>Image Gallery</p>
+            {loading ? (
+                <LoadingSpinner />
+            ) : (
+                <div>
+                    {/* Your content here */}
+                    <DndContext
+                        collisionDetection={closestCenter}
+                        onDragEnd={onDragEnd}>
+                        <SortableContext items={items} strategy={horizontalListSortingStrategy}>
+                            <div className='w-fit p-4 grid grid-cols-2 md:grid-cols-4 m-auto gap-4'>
+                                {items.map(cardItem => (
+                                    <SortableCard key={cardItem.id} cardItem={cardItem} />
+                                ))}
+                            </div>
+                        </SortableContext>
+                    </DndContext>
+                </div>
+            )}
         </div>
-      <div className="image-gallery flex flex-wrap gap-4">
-       
-        <DndContext
-          collisionDetection={closestCenter}
-          onDragEnd={onDragEnd}>
-          <SortableContext items={filteredItems} strategy={horizontalListSortingStrategy}>
-            <div className='w-fit p-4 grid grid-cols-2 md:grid-cols-4 m-auto gap-4'>
-              {filteredItems.map(cardItem => (
-                <SortableCard key={cardItem.id} cardItem={cardItem} />
-              ))}
-            </div>
-          </SortableContext>
-        </DndContext>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ImageCard;
---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+const LoadingSpinner = () => {
+  const [currentColor, setCurrentColor] = useState('bg-red-500'); // Initial color
+  const colors = ['bg-red-500', 'bg-blue-500', 'bg-green-500', 'bg-yellow-500']; // List of colors
+  let colorIndex = 0; // Current color index
+
+  useEffect(() => {
+    // Function to change the color every 500 milliseconds
+    const changeColor = () => {
+      setCurrentColor(colors[colorIndex]);
+      colorIndex = (colorIndex + 1) % colors.length;
+    };
+
+    // Set up an interval to change the color
+    const colorInterval = setInterval(changeColor, 500);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(colorInterval);
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className={`animate-spin rounded-full border-t-4 border-b-4 border-gray-900 h-16 w-16 ${currentColor}`}></div>
+      <p className='font-bold text-lg'>Loading Images...</p>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
