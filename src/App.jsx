6523ee8afd3cd9e7c49923f7989edec39b293028
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-09-21 18:30:35 +0100

    added loading component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
-    <Header />
       <ToastContainer position="top-right" autoClose={3000} />
       <LoginForm />
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 
 const Header = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Initialize with false for logged out state
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   // Function to toggle login/logout status
   const toggleLogin = () => {
@@ -14,19 +14,9 @@ const Header = () => {
       <div className="container mx-auto flex items-center justify-between">
         <a href='/' className="text-white text-2xl font-semibold">Image Gallery</a>
         <nav className="space-x-4">
-          {/* Display "Sign In" or "Sign Out" based on isLoggedIn state */}
-          {isLoggedIn ? (
-            <button
-              className="text-white hover:text-gray-200"
-              onClick={toggleLogin}
-            >
-              Sign Out
-            </button>
-          ) : (
             <a href="/" className="text-white hover:text-gray-200">
-              Sign In
+              Sign Out
             </a>
-          )}
         </nav>
       </div>
     </header>
